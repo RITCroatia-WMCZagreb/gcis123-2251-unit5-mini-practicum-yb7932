6ca3662cb73dedb4925e330987619116fb86863f
yb7932 <yb7932@g.rit.edu>
2025-10-15 13:00:22 +0200

    Add files via upload

--- pi_tester.py
+++ pi_tester.py
@@ -1,18 +1,54 @@
 '''
-@ASSESSME.USERID: userID
+@ASSESSME.USERID: yb7932
 @ASSESSME.AUTHOR: author, list of authors
 @ASSESSME.DESCRIPTION: MiniPractical
 @ASSESSME.ANALYZE: YES
 '''
 
 # The  thecima values of PI
-PI_VALUE = ""
+PI_VALUE = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679"
 
 def pi_tester():
-    pass
+    correct_counte = 0
+    print("Input PI decimals one at time:")
+    for i in range(len(PI_VALUE)):
+        digit_number = 1
+    input1 = input("Enter the " + str(digit_number)"number of pi: ")
+    if input1 == PI_VALUE [1]:
+        correct_counte = ""
+
+    else: 
+       print("Incorrect digit. The correct digit is:"+ PI_VALUE [i] + ".")
+       print("Number of correct decimal digit:"+ str(correct_counte))
+    
+
+    return correct_counte
+
+
+
+
+
 
 def display_score(score):
-    pass
+    if score >= 4:
+      title = "PI Neophyte"
+    elif score >= 9:
+        title = "PI Novice"
+    else score>= 24:
+     if title >= "PI Journeyman"
+        score>= 25:
+    if  title >= "PI Enthusiast"
+    if  score >= 50:
+        title = "PI Connoisseur"
+        score =
+    if  title>= "PI Expert"
+
+    print("Your title:" title)
+    
+
+
+     
+    
 
 def main():
     pass
